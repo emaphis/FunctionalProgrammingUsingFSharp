3578c5fe9dc903e7dc8ee887a8f1bc9da2dcb55e
emaphis <emaphis85@gmail.com>
2024-06-03 23:02:58 -0400

    More basics

---
+++ src/Chapter02/Section02_04.fsx
@@ -0,0 +1,38 @@
+﻿// 2.4 If-then-else expression
+
+// Has the form:
+// if exp1 then exp2 else exp3
+// where exp1 is an expression of type `bool`
+// and exp2 exp3 are expressions of the same type
+
+// If expressions are useful ins situations aere splitting
+// into cases is inconvenient.
+
+
+//  a function on strings that adjusts a string to even size
+// by putting a space character in front of the string if
+// the size is odd.
+
+let even n = n % 2 = 0
+
+let adjString s =
+    if even (String.length s)
+    then s else " " + s
+
+// val adjString: s: string -> string
+
+adjString "123"
+// val it: string = " 123"
+
+adjString "1234"
+// val it: string = "1234"
+
+
+// Pattern matching is to be prefered as highlighted by this
+// less readable GCD definition
+
+let rec gcd(m, n) =
+    if m=0 then n
+    else gcd (n % m, m)
+
+// val gcd: m: int * n: int -> int
---
+++ src/Chapter02/Section02_05.fsx
@@ -0,0 +1,35 @@
+﻿// 2.5 Overloaded functions and operators
+
+// + can be used to add floats, integers, and concatenaate strings
+
+// If the type can be inferred from the context.
+
+// if not the type can be specified 
+
+// obvious interpretation.
+
+let squareI x = x * x
+// val square: x: int -> int
+
+// specifying the parameter type
+let squareF1 (x: float) = x * x
+// val squareF1: x: float -> float
+
+// specifying the return type
+let squareF2 x = x * x : float
+// val squareF2: x: float -> float
+
+// or chosing a to build from a typed function:
+// abs, acos, atan, atan2, ceil, cos, cosh, exp, floor, log
+// log10, pow, pown, round, sin, sinh, sqrt, tan, tanh
+
+// abs is overloaded
+
+abs -1
+// val it: int = 1
+
+abs -1.0
+// val it: float = 1.0
+
+abs -1.0f
+// val it: float32 = 1.0
