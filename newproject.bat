f19416b7640a8557961c94515904d5640b4b6c33
emaphis <emaphis85@gmail.com>
2024-06-07 15:07:46 -0400

    Add chapter 3 projects.

--- newproject.bat
+++ newproject.bat
@@ -1,9 +1,9 @@
-dotnet new console -lang F# -o src/Chapter02
-dotnet sln add src/Chapter02/Chapter02.fsproj
-dotnet new xunit -lang F# -o tests/Chapter02Tests
-dotnet sln add tests/Chapter02Tests/Chapter02Tests.fsproj
-cd tests/Chapter02Tests
-dotnet add reference ../../src/Chapter02/Chapter02.fsproj
+dotnet new console -lang F# -o src/Chapter03
+dotnet sln add src/Chapter03/Chapter03.fsproj
+dotnet new xunit -lang F# -o tests/Chapter03Tests
+dotnet sln add tests/Chapter03Tests/Chapter03Tests.fsproj
+cd tests/Chapter03Tests
+dotnet add reference ../../src/Chapter03/Chapter03.fsproj
 dotnet add package Xunit
 dotnet add package FsUnit.XUnit
 dotnet add package Swensen.Unquote
