aa1d27adaf49626579825e40c8c158f1ff3144b3
emaphis <emaphis85@gmail.com>
2024-06-25 16:08:08 -0400

    Exceptions and option types

---
+++ src/Chapter03/Section_03_10.fs
@@ -0,0 +1,69 @@
+﻿// 3.10 Exceptions
+
+module Section_03_10
+
+(*
+    Exceptions terminate the evaluation of functions when
+    an error condition is detected.
+*)
+
+let solve (a, b, c) =
+    let disc = b*b - 4.0 * a * c
+    if disc < 0.0 || a = 0.0
+    then  failwith "discriminant is negative or a=0.0"
+    else 
+        let sqrtD = sqrt disc
+        ((-b + sqrtD) / (2.0 * a), (-b - sqrtD) / (2.0 * a))
+
+(*
+    It's possible to `catch` an exception using a 'try...with`
+    expression
+*)
+
+let solveText eq =
+    try
+        string(solve eq)
+    with
+    | solve  -> "No solutions"
+
+// val solveText: float * float * float -> string
+
+// `solveText` returns the solution as text if it has a solution
+
+let solution0 = solveText (1.0, 1.0, -2.0)
+// val solution0: string = "(1, -2)"
+
+
+// It returns "No solutions" if the equation has no solutions
+
+let solution1 = solveText (1.0, 0.0, 1.0)
+// val solution1: string = "No solutions"
+
+// `failwith s` will raise the exception `Failure s` which can be
+// caught.
+
+// The function `mkQ`, for example, will call `failwith` in the case
+// of a division by zero:
+
+let mkQ =
+    function
+    | (_,0)  -> failwith "Division by zero"
+    | pr ->  (fst pr) / (snd pr)
+
+
+let output =
+    try
+        (mkQ(2, 0)).ToString()
+    with
+    | Failure s -> s
+
+// val output: string = "Division by zero"
+
+// A 'try...with' expression has the form:
+//      try e with match
+// where 'e' us ab expression and 'match' is a construct or:
+//      | pat1 -> e1
+//      | pat2 -> e2
+//      ...
+//      | patn -> en
+// with patterns `pat1 ... patn` and expressions `e1 ... en`
---
+++ src/Chapter03/Section_03_11.fs
@@ -0,0 +1,84 @@
+﻿// 3.11 Partial functions. The option type
+
+module Section_03_11
+
+(*
+    A function 'f' is a 'partial' functionon a set 'A' if the domaine of 'f' is a
+    proper subset of 'A.'  So the factorial function is a partial function
+    of the set of integers because it is undefined on negative integers.
+
+    F# offers three ways of handling arguments where the function is undefined:
+
+    1. The evaluation of the function does not terminate.
+
+    2. The evaluation of the function is terminated by raising an exception.
+
+    3. The evaluation of the function returns a special result.
+
+    The third option can used the predefubed 'option' tyoe:
+*)
+
+//type 'a option = None | Some of 'a
+
+(*
+    Where 'None' is returned as the result of undefined function evaluations.
+    and 'Some v' when the function evaluation has the value 'v'
+*)
+
+// The constructor 'Some' is polymorphic and can be aplied to any value type
+
+let res1 = Some false
+// val res1 bool option = Some false
+
+// the library function 'get' removes the 'Some' from the falue.
+
+let fun1 = Option.get
+// val fun1: ('a option -> 'a)
+
+let res2: bool = Option.get(res1)
+// val res2: bool = false
+
+let res3 = Option.get(Some (1, "a"))
+// val res3: int * string = (1, "a")
+
+let res4 = Option.get(Some 1)
+// val res4: int = 1
+
+//let res5 = Option.get None + 1
+// System.ArgumentException: The option value was None
+
+// Redefined 'fact' function
+
+let rec fact n =
+ if n = 0 then 1
+ else n * fact(n - 1) 
+
+let optFact n = if n < 0 then None else Some(fact n)
+
+// val optFact: n: int -> int option
+
+// now 'optFact' always returns a value given an integer
+
+let res6 = optFact 5
+// val res6: int option = Some 120
+
+let res7 = optFact -2
+// val res7: int option = None
+
+
+// optFact assume the existence of 'fact' but 'optFact' can fe defined
+// independently
+
+let rec optFact' = function
+    | 0     -> Some 1
+    | n when n > 0 -> Some(n * Option.get(optFact'(n-1)))
+    | _     -> None;
+
+// val optFact': _arg1: int -> int option
+
+
+let res8 = optFact' 5
+// val res8: int option = Some 120
+
+let res9 = optFact' -2
+// val res9: int option = None
