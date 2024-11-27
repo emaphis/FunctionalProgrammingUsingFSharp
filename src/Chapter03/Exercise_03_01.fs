e4425a0f7ff8d051002ffe5f2cc6097a8c2d1605
emaphis <emaphis85@gmail.com>
2024-11-27 00:44:01 -0500

    Finish Exersize 3.1

---
+++ src/Chapter03/Exercise_03_01.fs
@@ -0,0 +1,63 @@
+﻿module Chapter03.Exercise_03_01
+
+(*
+    Exercise 3.1
+
+    A time of day can be represented as a triple (hours, minutes, f) where f is either AM or PM
+    – or as a record. Declare a function to test whether one time of day comes before another. For
+    example,(11,59,"AM") comes before (1,15,"PM"). Make solutions with triples as well
+    as with records. Declare the functions in infix notation.
+*)
+
+// should sort in proper order
+type Meridian = AM | PM
+
+
+// Time as a triple
+
+/// check if time1 is less than time2 with times represented as triples: hours * minutes * meridian
+let lessthanT time1 time2 =
+    let (hour1, minute1, meridian1) = time1
+    let (hour2, minute2, meridian2) = time2
+    (meridian1, hour1, minute1) < (meridian2, hour2, minute2)
+
+let time1 = (11, 59, AM)
+let time2 = (1, 15, PM)
+
+let bool1 = lessthanT time1 time2
+let bool2 = not (lessthanT time2 time2)
+
+
+// Time as a record
+
+type Time =
+    {
+        Hours : int
+        Minutes : int
+        Meridian : Meridian
+    }
+
+
+/// check if time1 is less than time2, with Time represented as a record
+let lessthanR (time1: Time) (time2: Time) =
+    lessthanT (time1.Hours, time1.Minutes, time1.Meridian) (time2.Hours, time2.Minutes, time2.Meridian)
+   
+
+/// Create a Time record from a time triple
+let time (hours, minutes, meridian) =
+    { Hours = hours; Minutes = minutes; Meridian = meridian}
+
+
+let time3 = time time1
+let time4 = time time2
+
+let bool3 = lessthanR time3 time4
+let bool4 = not(lessthanR time4 time3)
+
+
+/// Check if time1 is less than time2
+let (<.) time1 time2 = lessthanR time1 time2
+
+
+let bool5 = time3 <. time4
+let bool6 = not(time4 <. time3)
--- tests/Chapter03Tests/Exercise_03_01_Tests.fs
+++ tests/Chapter03Tests/Exercise_03_01_Tests.fs
@@ -4,39 +4,26 @@ open System
 open Xunit
 open Swensen.Unquote
 
-open Chapter03.Execise_03_01
-open Section_03_07
+open Chapter03.Exercise_03_01
+open Exercise_03_01
+
+let time1 = (11, 59, AM)
+let time2 = (1, 15, PM)
 
 [<Fact>]
-let ``Test Example 3.7: Rational number canc`` () =
-   test <@ canc(2, 2) = (1, 1)  @>
-   test <@ canc(-2, 2) = (-1, 1)  @>
-   test <@ canc(1, 1) = (1, 1) @>
-   test <@ canc(3, 12) = (1, 4) @>
-   test <@ canc(12, 4) = (3, 1) @>
+let ``Test Exercise 3.1: time as a triple`` () =
+   test <@ lessthanT time1 time2 = true  @>
+   test <@ not (lessthanT time2 time2) = true  @>
+
+let time3 = time time1
+let time4 = time time2
 
 [<Fact>]
-let ``Test Example 3.7, mkQ``() =
-    test <@ mkQ(5,10) = (1,2) @>
-    test <@ mkQ(2, -3) = (-2,3) @>
-    raises<System.Exception> <@ mkQ(5, 0) @>
+let ``Test Exercise 3.1, Time record``() =
+    test <@ lessthanR time3 time4 = true @>
+    test <@ not(lessthanR time4 time3) = true @>
  
- 
 [<Fact>]
-let ``Test Example 3.7, toString``() =
-    let rat = (-1, 3)
-    test <@ toString rat = "-1/3" @>
-
-
-[<Fact>]
-let ``Test Example 3.7, operators``() =
-    let rat1 = (1, 2)
-    let rat2 = (2, 3)
-    let rat3 = (1, 1)
-    test <@ rat1 .+. rat2 = (7, 6) @>
-    test <@ rat1 .-. rat2 = (-1, 6) @>
-    test <@ rat1 .*. rat3 = (1, 2) @>
-    test <@ rat1 ./. rat2 = (3, 4) @>
-    test <@ rat1 ./. rat3 = (1, 2) @>
-    test <@ rat1 .=. rat2 = false @>
-    test <@ rat1 .=. (1, 2) = true @>
+let ``Test Exercise 3.1, LT operator``() =
+    test <@ time3 <. time4 = true @>
+    test <@ not(time4 <. time3) = true @>
