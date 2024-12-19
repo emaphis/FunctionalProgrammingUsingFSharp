d6704777cba59c0d33cfad30bf5b67b7572caa35
emaphis <emaphis85@gmail.com>
2024-12-19 17:15:07 -0500

    New project for Chapter 4

--- newproject.bat
+++ newproject.bat
@@ -1,9 +1,9 @@
-dotnet new console -lang F# -o src/Chapter03
-dotnet sln add src/Chapter03/Chapter03.fsproj
-dotnet new xunit -lang F# -o tests/Chapter03Tests
-dotnet sln add tests/Chapter03Tests/Chapter03Tests.fsproj
-cd tests/Chapter03Tests
-dotnet add reference ../../src/Chapter03/Chapter03.fsproj
+dotnet new console -lang F# -o src/Chapter04
+dotnet sln add src/Chapter04/Chapter04.fsproj
+dotnet new xunit -lang F# -o tests/Chapter04Tests
+dotnet sln add tests/Chapter04Tests/Chapter04Tests.fsproj
+cd tests/Chapter04Tests
+dotnet add reference ../../src/Chapter04/Chapter04.fsproj
 dotnet add package Xunit
 dotnet add package FsUnit.XUnit
 dotnet add package Swensen.Unquote
---
+++ src/Chapter04/README.md
@@ -0,0 +1,16 @@
+# Chapter 4 - Lists
+
+Lists are at the core of functional programming. A large number of applications can be mod
+elled and implemented using lists.
+
+This chapter introduces the List consept including: list values, patterns and basic operations, and we study a collection of recursion schemas over lists.
+
+At the end of the chapter  model-based approach to functional programming is introduced
+
+ 4 Lists
+ 4.1 The concept of a list
+ 4.2 Construction and decomposition of lists
+ 4.3 Typical recursions over lists
+ 4.4 Polymorphism
+ 4.5 The value restrictions on polymorphic expressions
+ 4.6 Examples.A model-based approach
