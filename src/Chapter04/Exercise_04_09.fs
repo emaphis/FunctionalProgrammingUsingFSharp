4957a6f1cd21aaf5f336bb394e36ad260e36440a
emaphis <emaphis85@gmail.com>
2025-01-01 14:16:03 -0500

    Changed `zip` to use `reverse` instead of `concat`.

--- src/Chapter04/Exercise_04_09.fs
+++ src/Chapter04/Exercise_04_09.fs
@@ -12,6 +12,10 @@ The function should raise an exception if the two lists are not of equal length.
 *)
 
 
+
+(*   Original method
+    But I didn't like using concatenation, so I rewrote it using `reverse`
+
 exception DifferentLengths
 
 /// Compute length of lists
@@ -26,25 +30,52 @@ let length xs : int =
 
 /// Zip two lists
 let zip xs ys  =
-    let rec loop (xs, ys, zs) =
-        if length xs <> length ys then raise DifferentLengths
-        else
+    if length xs <> length ys then raise DifferentLengths
+    else
+        let rec loop (xs, ys, zs) =
             match (xs, ys, zs) with
             | x::xs, y::ys, zs -> loop (xs, ys, (zs @ [(x, y)]))
             | _, _, zs -> zs
 
-    loop (xs, ys, [])
+        loop (xs, ys, [])
 
-
-(*
-length [] = 0
-length [0] = 1
-length [0;1;2] = 3
+// val zip: xs: 'a list -> ys: 'b list -> ('a * 'b) list
 *)
 
+
+exception DifferentLengths
+
+/// Reverse a list
+let reverse list =
+    let rec loop acc = function
+        | [] -> acc
+        | x::xs -> loop (x::acc) xs
+    loop [] list
+
+
+/// Zip two lists
+let zip (pairs : 'a list * 'b list) =
+    let rec loop (aux: ('a * 'b) list, lst) =
+        match lst with
+        | x::xs, y::ys -> loop ((x,y)::aux, (xs, ys))
+        | _::_, [] -> raise DifferentLengths
+        | [], _::_ -> raise DifferentLengths
+        | [], [] -> reverse aux
+
+    loop ([], pairs)
+
+//val zip: 'a list * 'b list -> ('a * 'b) list
+
+
 (*
-zip [] [] = []
-zip ["x0"] ["y0"] = [("x0", "y0")]
-zip ["x0"; "x1"] ["y0"; "y1"] =  [("x0", "y0"); ("x1", "y1")]
-zip [0; 2; 4] [1; 3; 5] = [(0, 1); (2, 3); (4, 5)]
+zip ([], []) = []
+zip (["x0"], ["y0"]) = [("x0", "y0")]
+zip (["x0"; "x1"], ["y0"; "y1"])  =  [("x0", "y0"); ("x1", "y1")]
+zip ([0; 2; 4], [1; 3; 5]) = [(0, 1); (2, 3); (4, 5)]
+*)
+(*
+reverse [] = []
+reverse [0] = [0]
+reverse [0;1] = [1;0]
+reverse [0;1;2] = [2;1;0]
 *)
--- tests/Chapter04Tests/Exercise_04_09_Test.fs
+++ tests/Chapter04Tests/Exercise_04_09_Test.fs
@@ -7,15 +7,18 @@ open Swensen.Unquote
 open Chapter04.Exercise_04_09
 
 [<Fact>]
-let ``Test Exercise 4.9 length`` () =
-    test <@ length [] = 0 @>
-    test <@ length [0] = 1 @>
-    test <@ length [0;1;2] = 3 @>
+let ``Test Exercise 4.9 reverse`` () =
+    test <@ reverse [] = [] @>
+    test <@ reverse [0] = [0] @>
+    test <@ reverse [0; 1] = [1; 0] @>
+    test <@ reverse [0; 1; 2; 3] = [3; 2; 1; 0] @>
 
 
 [<Fact>]
 let ``Test Exercise 4.9 zip`` () =
-    test <@ zip [] [] = [] @>
-    test <@ zip ["x0"] ["y0"] = [("x0", "y0")] @>
-    test <@ zip ["x0"; "x1"] ["y0"; "y1"] =  [("x0", "y0"); ("x1", "y1")] @>
-    test <@ zip [0; 2; 4] [1; 3; 5] = [(0, 1); (2, 3); (4, 5)] @>
+    test <@ zip ([], []) = [] @>
+    test <@ zip (["x0"], ["y0"]) = [("x0", "y0")] @>
+    test <@ zip (["x0"; "x1"], ["y0"; "y1"]) =  [("x0", "y0"); ("x1", "y1")] @>
+    test <@ zip ([0; 2; 4], [1; 3; 5]) = [(0, 1); (2, 3); (4, 5)] @>
+    raises<DifferentLengths> <@ zip ([], [1]) @>
+    raises<DifferentLengths> <@ zip ([1], []) @>
