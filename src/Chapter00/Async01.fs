cca02da541f78d2310a7c918884fb8d58d043e10
emaphis <emaphis85@gmail.com>
2025-01-21 23:55:46 -0500

    Async example

---
+++ src/Chapter00/Async01.fs
@@ -0,0 +1,33 @@
+﻿// Async examples
+
+open System
+open System.IO
+
+let owl () =
+  async {
+    Threading.Thread.Sleep 2000
+    return "Owl"
+  }
+
+let devOwl =
+  async {
+    printf "Dev"
+    let! owl1 = owl()
+    printfn "$A" owl1
+  }
+
+Async.Start devOwl
+Threading.Thread.Sleep 1000
+printfn "rommsen"
+
+
+
+[<EntryPoint>]
+let main argv =
+    Directory.SetCurrentDirectory($"C:\\temp\\")
+    let path = "Hello.txt"
+    //printTotalFileBytesUsingAsync path
+   // |> Async.RunSynchronously
+
+    Console.Read() |> ignore
+    0
